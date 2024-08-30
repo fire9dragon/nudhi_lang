772f69a8133d9dd460bf33e7ee3f0388177a8766
fire9dragon <168269207+fire9dragon@users.noreply.github.com>
2024-08-30 10:09:23 +0200

    Delete target directory

--- target/debug/deps/nudhi_lang_interpreter.d
+++
@@ -1,5 +0,0 @@
-C:\Users\umarm\OneDrive\code\rust\projects\nudhi-lang-interpreter\target\debug\deps\nudhi_lang_interpreter.exe: src/main.rs
-
-C:\Users\umarm\OneDrive\code\rust\projects\nudhi-lang-interpreter\target\debug\deps\nudhi_lang_interpreter.d: src/main.rs
-
-src/main.rs:
--- target/debug/nudhi-lang-interpreter.d
+++
@@ -1,1 +0,0 @@
-C:\Users\umarm\OneDrive\code\rust\projects\nudhi-lang-interpreter\target\debug\nudhi-lang-interpreter.exe: C:\Users\umarm\OneDrive\code\rust\projects\nudhi-lang-interpreter\src\main.rs
